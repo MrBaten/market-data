4a12672bbded37ecfbebae337a35176f8d212d64
Mr Baten <84617592+MrBaten@users.noreply.github.com>
2023-12-06 03:09:21 +0600

    add fa

--- build_files.sh
+++ build_files.sh
@@ -5,6 +5,7 @@ source mrbaten/env/bin/activate
 # Install dependencies
 python3 -m pip install -r requirements.txt
 
+python3 -m pip install django
 # Run Django management commands
 python3 manage.py collectstatic --noinput --clear
 
